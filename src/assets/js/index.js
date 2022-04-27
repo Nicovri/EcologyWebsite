7be0e658cc572360722a058faf4a427c090c8ca5
Nicolas Vrignaud <nicoovri@gmail.com>
2022-04-27 19:52:21 +0200

    Working on the animations. Understood and applied the main methods.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,21 @@
+window.onload = function () {
+  setTimeout(function () {
+    document.body.style.opacity = "100";
+  }, 500);
+};
 
+var improve1 = document.getElementsByClassName("grid-item-improve-1");
+
+// Change the style of improve1 depending of if its visible on the screen or not (on scroll down only)
+// On scroll back up, make the animations reverse??
+window.addEventListener("scroll", function () {
+  if (improve1[0].getBoundingClientRect().top < window.innerHeight) {
+    improve1[0].style.backgroundColor = "red";
+    improve1[0].style.transition = "1s";
+  }
+});
+window.addEventListener("scroll", function () {
+  if (improve1[0].getBoundingClientRect().top > window.innerHeight) {
+    improve1[0].style.backgroundColor = "white";
+  }
+});
